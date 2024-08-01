e87383e0bf88278c70014c414db2c2cd99076af7
rlazar25 <119755392+rlazar25@users.noreply.github.com>
2024-08-01 18:00:45 +0200

    Created navbar component, added in App.jsx and created style for action calssName

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom"
+import NavbarComponent from "./components/NavbarComponent"
 
 function App() {
 
   return (
     <div>
+      <NavbarComponent />
       <Outlet />
     </div>
   )
---
+++ src/components/NavbarComponent.jsx
@@ -0,0 +1,29 @@
+import { NavLink } from "react-router-dom";
+
+function NavbarComponent(){
+
+    const navbarItems = [
+        {
+            name: 'Home',
+            url: '/',
+        },
+        {
+            name: 'Products',
+            url: '/products',
+        },
+        {
+            name: 'Users',
+            url: '/users',
+        }
+    ]
+
+    return(
+        <div className="flex gap-2 items-center justify-end text-white text-[1.2rem] bg-slate-600 p-[1rem]">
+            {navbarItems.map((item, index) => {
+               return (<NavLink to={item.url} key={index}>{item.name}</NavLink>)
+            })}
+        </div>
+    )
+}
+
+export default NavbarComponent;
